117968adfb523c3cb1cd6647a5f175aea987f510
kurohage <517326+kurohage@users.noreply.github.com>
2019-10-31 21:21:14 +0300

    solved.

--- functions.js
+++ functions.js
@@ -6,7 +6,7 @@
  * - returns undefined if no matching book is found
  ****************************************************************/
 function getBookById(bookId, booksArray) {
-  return books.find(book => book.id === bookId);
+  return booksArray.find(book => book.id === bookId);
 }
 
 /**************************************************************
@@ -18,7 +18,7 @@ function getBookById(bookId, booksArray) {
  ****************************************************************/
 function getAuthorByName(authorName, authors) {
   return authors.find(
-    author => author.name.toLowercase() === authorName.toLowercase()
+    author => author.name.toLowerCase() === authorName.toLowerCase()
   );
 }
 
@@ -29,10 +29,12 @@ function getAuthorByName(authorName, authors) {
  *    [{ author: <NAME>, bookCount: <NUMBER_OF_BOOKS> }]
  ****************************************************************/
 function bookCountsByAuthor(authors) {
-  return authors.map(auth => ({
-    author: author.name,
-    bookCount: author.books.length
-  }));
+  return authors.map(auth => {
+    return {
+      author: auth.name,
+      bookCount: auth.books.length
+    };
+  });
 }
 
 /**************************************************************
@@ -78,7 +80,7 @@ function titlesByAuthorName(authorName, authors, books) {
  * Note: assume there will never be a tie
  ****************************************************************/
 function mostProlificAuthor(authors) {
-  let prolificAuthor = authors[authors.length];
+  let prolificAuthor = authors[0];
 
   authors.forEach(author => {
     if (author.books.length > prolificAuthor.books.length) {
@@ -119,14 +121,18 @@ function mostProlificAuthor(authors) {
 function relatedBooks(bookId, authors, books) {
   const book = getBookById(bookId, books);
   let titles = [];
+  let tmp;
 
-  book.authors.forEach(
-    author =>
-      (titles = titles.concat(titlesByAuthorName(author.name, authors, books)))
-  );
+  book.authors.forEach(author => {
+    //tmp = titlesByAuthorName(author.name, authors, books);
+    titlesByAuthorName(author.name, authors, books).forEach(book => {
+      if (titles.indexOf(book) === -1) titles.push(book);
+    });
+  });
 
   /* BONUS */
   // titles = [...new Set(titles)];
+  console.log(titles);
 
   return titles;
 
